c7345a07a1da51e3641c9399a18a2b2e2ca9f9e6
w2xi <43wangxi@gmail.com>
2022-07-23 23:24:57 +0800

    feat: 登录页面UI

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
 
-import './assets/main.css'
+import 'element-plus/dist/index.css'
+import './assets/styles/normalize.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 
---
+++ src/views/Login.vue
@@ -0,0 +1,104 @@
+<script setup>
+import { ref } from 'vue';
+import { Lock, User } from '@element-plus/icons-vue';
+
+const formRef = ref(null);
+const loading = ref(false);
+const form = ref({
+  username: '',
+  password: '',
+});
+const formRules = ref({
+  username: [
+    { required: true, message: 'please enter username', trigger: 'blur' },
+  ],
+  password: [
+    { required: true, message: 'please enter password', trigger: 'blur' },
+  ],
+});
+
+function submit() {
+  formRef.value.validate(valid => {
+    if (valid) {
+      // ...
+    }
+  })
+}
+</script>
+
+<template>
+  <div class="login-container">
+    <el-form 
+      ref="formRef" 
+      :model="form" 
+      :rules="formRules"
+    >
+      <el-form-item prop="username">
+        <el-input v-model="form.username" placeholder="Username">
+          <template #prepend>
+            <el-button :icon="User" />
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input type="password" v-model="form.password" placeholder="Password">
+          <template #prepend>
+            <el-button :icon="Lock" />
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="login-btn" type="primary" @click="submit" :loading="loading">
+          SIGN IN
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login-container {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #2c3338;
+}
+
+.el-form {
+  width: 22rem;
+}
+.el-form-item {
+  :deep(.el-form-item__error) {
+    font-size: 16px;
+  }
+}
+
+.el-input {
+  padding: 0;
+}
+
+.el-input {
+  :deep(.el-input-group__prepend) {
+    background-color: #363b41;
+    box-shadow: none;
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+  }
+  :deep(.el-input__wrapper) {
+    padding: 0;
+  }
+  :deep(.el-input__inner) {
+    padding: 1.7rem 1rem;
+    color: #eee;
+    background-color: #3b4148;
+  }
+}
+
+.login-btn {
+  width: 100%;
+  padding: 1.6rem 0;
+  margin-top: 5px;
+}
+</style>
