a45f9d8feb4937f98a6c21b3935e726773b891b6
w2xi <43wangxi@gmail.com>
2022-07-28 23:37:39 +0800

    chore: VTree 递归组件 新增 Tree Demo

---
+++ src/components/VTree.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  data: {
+    type: Array,
+    default() {
+      return [];
+    }
+  }
+});
+</script>
+
+<template>
+  <div class="tree">
+    <div 
+      class="tree-item"
+      v-for="item of data"
+    >
+      <div class="tree-label">
+        <i class="icon" v-if="item.children && item.children.length">
+          <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M384 192v640l384-320.064z"></path></svg>
+        </i>
+        {{ item.label }}
+      </div>
+      <VTree :data="item.children" v-if="item.children && item.children.length" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tree-item {
+  padding-left: 20px;
+  .tree-label {
+    padding: 2px 0;
+  }
+  .tree-label:hover {
+    background-color: #efefef;
+  }
+}
+.icon {
+  font-size: 12px;
+  color: #a8abb2;
+}
+svg {
+  height: 1em;
+  width: 1em;
+}
+</style>
---
+++ src/views/Tree.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { ref } from 'vue';
+import VTree from '../components/VTree.vue'
+
+const list = ref([
+  {
+    label: 'Level one 1',
+    children: [
+      {
+        label: 'Level two 1-1',
+        children: [
+          {
+            label: 'Level three 1-1-1',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Level one 2',
+    children: [
+      {
+        label: 'Level two 2-1',
+        children: [
+          {
+            label: 'Level three 2-1-1',
+          },
+        ],
+      },
+      {
+        label: 'Level two 2-2',
+        children: [
+          {
+            label: 'Level three 2-2-1',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Level one 3',
+    children: [
+      {
+        label: 'Level two 3-1',
+        children: [
+          {
+            label: 'Level three 3-1-1',
+          },
+        ],
+      },
+      {
+        label: 'Level two 3-2',
+        children: [
+          {
+            label: 'Level three 3-2-1',
+          },
+        ],
+      },
+    ],
+  },
+]);
+</script>
+
+<template>
+  <div class="tree-container">
+    <VTree :data="list" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
