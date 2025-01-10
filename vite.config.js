ed2fd2d43646b89f096a97c57a85f4206c19b15d
w2xi <43wangxi@gmail.com>
2025-01-10 16:13:49 +0800

    chore: update configuration

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.50.76:9000',
+        changeOrigin: true,
+      }
+    }
   }
 })
